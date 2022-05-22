054f039e6f407fd559d6e265cce5a12b733dca8d
Abhishek Singh <abhiwebdev2718@gmail.com>
2022-05-22 10:10:27 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express=require('express'),
+      app=express(),
+      port=process.env.PORT || 8080,
+      server=require('http').createServer(app),
+      { Server } = require("socket.io"),
+      io = new Server(server,{
+          cors:{
+            orgin:"*",
+            method:["GET","POST"],
+            allowedHeaders:["my-custom-header"],
+            credentials:true
+          }
+      });
+      io.on('connection',socket=>{
+        socket.on('join-room',room=>{
+          console.log(room);
+          socket.join(room);
+        });
+        //   socket.on('chat',payload=>{
+        //       socket.to(payload.room).emit('chat',payload.message);
+        //   });
+        //   socket.on('cut_call',payload=>{
+        //     socket.to(payload.room).emit('cut_call',payload);
+        //   })
+      });
+     
+      
+      server.listen(port,()=>{
+          console.log(`servering on ${port}`);
+      });
+     
